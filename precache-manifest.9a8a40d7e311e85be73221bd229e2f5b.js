c1e2f1a414b1f1180ad6a2ce34be0bdf1f3da547
Hiren13190 <hiren@sixberries.com>
2020-08-22 14:30:25 +0530

    Updates

---
+++ precache-manifest.9a8a40d7e311e85be73221bd229e2f5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc79d1356edaebdb4ae42363545bf29",
+    "url": "/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "16d77ba8e8c3901606c2",
+    "url": "/reactlivewebsite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "befd7e57ebeefcf5526a",
+    "url": "/reactlivewebsite/static/js/2.ef642563.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactlivewebsite/static/js/2.ef642563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16d77ba8e8c3901606c2",
+    "url": "/reactlivewebsite/static/js/main.698012fe.chunk.js"
+  },
+  {
+    "revision": "77463ddd60a983fc4e65",
+    "url": "/reactlivewebsite/static/js/runtime-main.ecdc915d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactlivewebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
